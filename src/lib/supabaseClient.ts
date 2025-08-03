ddcabb749d7bfda5d31ea5650a9daae894a72198
Oleg <gritsyk.2000@gmail.com>
2025-08-03 19:12:29 +0300

    Initial commit

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://sasongcwqfxxolyxbkbm.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhc29uZ2N3cWZ4eG9seXhia2JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MDM3MTksImV4cCI6MjA2OTQ3OTcxOX0.imvFkJ_7JEDbTIZbtkLC2DQiA3zHNFm4NpKzbIkgKOo";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
